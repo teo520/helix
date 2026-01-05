740ab07cc25ab74ae41c4cc300e73acde2cafa1d
TeoLau <p-o@qq.com>
2026-01-05 17:58:44 +0800

    for termux deb build

--- contrib/hx_launcher.sh
+++ contrib/hx_launcher.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-HELIX_RUNTIME=/usr/lib/helix/runtime exec /usr/lib/helix/hx "$@"
+HELIX_RUNTIME=/data/data/com.termux/files/usr/lib/helix/runtime exec /data/data/com.termux/files/usr/lib/helix/hx "$@"
